393f4975cb26075c8654e66efce3225745bde0b1
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-02 23:09:54 +0300

    get and post controllers

--- src/main/java/com/annalis/sample/api/controller/CatController.java
+++ src/main/java/com/annalis/sample/api/controller/CatController.java
@@ -1,20 +1,31 @@
 package com.annalis.sample.api.controller;
 
 import com.annalis.sample.api.domain.Cat;
+import com.annalis.sample.api.service.CatService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CatController {
+    @Autowired
+    private CatService catService;
     private Logger logger  = LoggerFactory.getLogger(CatController.class);
+//    @PostMapping("/cats")
+//    public Cat createCat(@RequestBody Cat cat){
+//        logger.info("Received cat: {}", cat);
+//        //do something about the cats
+//        return cat;
+//    }
+    @GetMapping("/cats")
+    public List<Cat> getAllCats(){
+        return catService.getAllCats();
+    }
     @PostMapping("/cats")
-    public Cat createCat(@RequestBody Cat cat){
-        logger.info("Received cat: {}", cat);
-        //do something about the cats
-        return cat;
+    public Cat  registerCat(@RequestBody Cat cat){
+        return catService.registerCat(cat);
     }
 }
