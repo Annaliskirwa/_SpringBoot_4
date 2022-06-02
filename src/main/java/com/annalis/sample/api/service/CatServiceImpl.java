750eeec53753c5ab0bf7d129d8f71c9e93a7c31f
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-02 22:03:16 +0300

    implemented all the service methods

--- src/main/java/com/annalis/sample/api/service/CatServiceImpl.java
+++ src/main/java/com/annalis/sample/api/service/CatServiceImpl.java
@@ -1,4 +1,34 @@
 package com.annalis.sample.api.service;
 
-public class CatServiceImpl {
+import com.annalis.sample.api.domain.Cat;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CatServiceImpl implements CatService{
+    @Override
+    public List<Cat> getAllCats() {
+        return null;
+    }
+
+    @Override
+    public Cat registerCat(Cat cat) {
+        return null;
+    }
+
+    @Override
+    public Cat updateCatDetails(Cat cat, Integer id) {
+        return null;
+    }
+
+    @Override
+    public String removeCatDetails(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Cat findCatById(Integer id) {
+        return null;
+    }
 }
