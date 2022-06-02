57187af418d3ae7673ade4ca1cdbc3c1a9ff926b
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-02 23:15:13 +0300

    put and delete controllers

--- src/main/java/com/annalis/sample/api/controller/CatController.java
+++ src/main/java/com/annalis/sample/api/controller/CatController.java
@@ -28,4 +28,16 @@ public class CatController {
     public Cat  registerCat(@RequestBody Cat cat){
         return catService.registerCat(cat);
     }
+    @PutMapping("/cats/{id}")
+    public Cat updateCatDetails(@PathVariable Integer id, @RequestBody Cat cat){
+        return catService.updateCatDetails(cat, id);
+    }
+    @GetMapping("/cats/{id}")
+    public Cat findCatById(@PathVariable Integer id){
+        return catService.findCatById(id);
+    }
+    @DeleteMapping("/cats/{id}")
+    public String  removeCatDetails(@PathVariable Integer id){
+        return catService.removeCatDetails(id);
+    }
 }
